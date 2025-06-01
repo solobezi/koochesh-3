111ef41072288a16104b7de16606de4ca0b5c414
solobezi <solobezii@gmail.com>
2025-06-01 21:35:12 +0330

    Create layout.php

---
+++ includes/layout.php
@@ -0,0 +1,13 @@
+<?php
+if (!isset($pageTitle)) $pageTitle = "Koochesh";
+?>
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?= $pageTitle ?> | مهاجرت</title>
+  <link href="https://fonts.googleapis.com/css2?family=Vazirmatn&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="/css/styles.css" />
+</head>
+<body>
