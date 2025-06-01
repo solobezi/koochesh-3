af7a0e3dd93b022bded3f8314305e4d1598b3fbf
solobezi <solobezii@gmail.com>
2025-06-01 21:35:43 +0330

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,10 @@
+<footer>
+  <div class="footer-container">
+    <!-- محتوای فوتر همونیه که تو index داشتی -->
+  </div>
+</footer>
+<script src="/js/cards.js"></script>
+<script src="/js/form.js"></script>
+</body>
+</html>
 
